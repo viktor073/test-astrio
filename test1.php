c96f993f8c2cd596b05cf8a1023db807fbcc0c03
viktor073 <viktor_karyakin@mail.ru>
2020-08-06 08:54:52 +0400

    Исправлена имя переменной. Поиск работает.

--- test1.php
+++ test1.php
@@ -1,48 +1,58 @@
 <?php
-	$categories = array(
-		array(
-			"id" => 1,
-			"title" => "Обувь",
-			"children" => array(
-				array(
-					"id" => 2,
-					"title" => "Ботинки",
-					'children' => array(
-						array('id' => 3, 'title' => 'Кожа'),
-						array('id' => 4, 'title' => 'Текстиль'),
-					),
+
+/**
+ * [$categories array value]
+ * @var array
+ */
+$categories = array(
+	array(
+		"id" => 1,
+		"title" => "Обувь",
+		"children" => array(
+			array(
+				"id" => 2,
+				"title" => "Ботинки",
+				'children' => array(
+					array('id' => 3, 'title' => 'Кожа'),
+					array('id' => 4, 'title' => 'Текстиль'),
 				),
-				array('id' => 5, 'title' => 'Кроссовки',),
+			),
+			array('id' => 5, 'title' => 'Кроссовки',),
+		)
+	),
+
+	array(
+		"id" => 6,
+		"title" => "Спорт",
+		'children' => array(
+			array(
+				'id' => 7,
+				'title' => 'Мячи'
 			)
-		),
+		)
+	),
+);
 
-		array(
-			"id" => 6,
-			"title" => "Спорт",
-			'children' => array(
-				array(
-					'id' => 7,
-					'title' => 'Мячи'
-				)
-			)
-		),
-	);
-
-	function searchCategory($categories, $id)
-	{
-		foreach($categories as $row){
-			if($row['id'] == $id){
-				return $row['title'];
-			}
-			if(isset($row['children'])){
-				$result = searchCategory($row['children'], $id);
-				if($result != null){
-					return $res;
-				}
-			}
-
+/**
+ * [searchCategory - search in array]
+ * @param  [array] $categories [array value]
+ * @param  [int] $id         [key array value]
+ * @return [string]             [value array]
+ */
+function searchCategory(array $categories, int $id): string
+{
+	foreach($categories as $row){
+		if($row['id'] == $id){
+			return $row['title'];
 		}
-	};
+		if(isset($row['children'])){
+			$result = searchCategory($row['children'], $id);
+			if(!is_null($result)){
+				return $result;
+			}
+		}
+	}
+};
 
-	$res = searchCategory($categories, 4);
-	var_dump($res);
+$result = searchCategory($categories, 3);
+var_dump($result);
